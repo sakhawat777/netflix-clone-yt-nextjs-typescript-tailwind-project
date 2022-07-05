bb63742a9d4e2914fad057a7c457c9c892b131fc
Md. Sakhawat Hossain <biplobbms@gmail.com>
2022-07-05 13:21:08 +0600

    Header Right slide done

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5615935be82403ae"; }
+/******/ 		__webpack_require__.h = function() { return "40dd9563c03b1527"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
