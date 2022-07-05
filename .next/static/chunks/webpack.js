aa1bb535276b0bf26439b63cb4e08f6b3c11372e
Md. Sakhawat Hossain <biplobbms@gmail.com>
2022-07-05 11:26:29 +0600

    Netflix Logo added

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a99b976e7afa7a6e"; }
+/******/ 		__webpack_require__.h = function() { return "67b6b595a6dc1aaa"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/Header.tsx
+++ components/Header.tsx
@@ -2,9 +2,19 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <div>
-           Header 
-        </div>
+        <header>
+            <div>
+            <img
+          src="https://rb.gy/ulxxee"
+          width={100}
+          height={100}
+          className="cursor-pointer object-contain"
+        />
+           </div>
+           <div>
+               
+           </div>
+        </header>
     );
 };
 
